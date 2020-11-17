0eed8a8c0d79a49284066b3221721afeb2986e48
Jeremy55-cyber <73191152+Jeremy55-cyber@users.noreply.github.com>
2020-11-17 14:18:23 -0500

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -110,26 +110,6 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(200)
     basic.clearScreen()
     basic.pause(500)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(50)
-    basic.clearScreen()
-    basic.pause(100)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        # # # # #
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(200)
-    basic.clearScreen()
-    basic.pause(100)
     for (let index = 0; index < 2; index++) {
         basic.showLeds(`
             . . . . .
@@ -143,26 +123,6 @@ input.onButtonPressed(Button.A, function () {
         basic.pause(100)
     }
     basic.pause(500)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(50)
-    basic.clearScreen()
-    basic.pause(100)
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        # # # # #
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(200)
-    basic.clearScreen()
-    basic.pause(100)
     for (let index = 0; index < 2; index++) {
         basic.showLeds(`
             . . . . .
@@ -174,6 +134,28 @@ input.onButtonPressed(Button.A, function () {
         basic.pause(50)
         basic.clearScreen()
         basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(200)
+        basic.clearScreen()
+        basic.pause(100)
+        for (let index = 0; index < 2; index++) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                . . . . .
+                `)
+            basic.pause(50)
+            basic.clearScreen()
+            basic.pause(100)
+        }
     }
     basic.pause(500)
     basic.showLeds(`
